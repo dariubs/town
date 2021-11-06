0cdee85f65af2aad3d7ff1de2032b7bfca47ca22
Dariush Abbasi <dariushem@yahoo.com>
2021-11-06 12:32:32 +0330

    add basic town table create function

--- town--0.0.1.sql
+++ town--0.0.1.sql
@@ -1,1 +1,16 @@
 \echo Use "CREATE EXTENSION base36" to load this file. \quit
+
+CREATE OR REPLACE FUNCTION create_town_table(t_name varchar(30))
+  RETURNS VOID
+  LANGUAGE plpgsql AS
+$func$
+BEGIN
+   EXECUTE format('
+      CREATE TABLE IF NOT EXISTS %I (
+       id serial PRIMARY KEY,
+       ts timestamp,
+       tag varchar(30),
+       data jsonb
+      )', t_name);
+END
+$func$;

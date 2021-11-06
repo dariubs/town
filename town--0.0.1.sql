28b94a87fce3b325884c7ab8058fe14e4bb9475b
Dariush Abbasi <dariushem@yahoo.com>
2021-11-06 21:53:16 +0330

    update tag column, rename to tags as an array

--- town--0.0.1.sql
+++ town--0.0.1.sql
@@ -1,4 +1,4 @@
-\echo Use "CREATE EXTENSION base36" to load this file. \quit
+\echo Use "CREATE EXTENSION town" to load this file. \quit
 
 CREATE OR REPLACE FUNCTION create_town_table(t_name varchar(30))
   RETURNS VOID
@@ -7,10 +7,11 @@ $func$
 BEGIN
    EXECUTE format('
       CREATE TABLE IF NOT EXISTS %I (
-       id bigserial PRIMARY KEY,
-       ts timestamp with time zone,
-       tag varchar(255),
-       data jsonb
-      )', t_name);
+       id BIGSERIAL PRIMARY KEY,
+       ts TIMESTAMP WITH TIME ZONE,
+       tags TEXT[],
+       data JSONB default ''{}''::jsonb
+      );
+      ', t_name, t_name);
 END
 $func$;
